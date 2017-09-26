4746c68b07d61a7b928210073ffea1a288255698
BrinkerVII <brinkervii@gmail.com>
2017-09-26 16:59:20 +0200

    Add base for event handling

--- src/app/event-processor.ts
+++ src/app/event-processor.ts
@@ -1,16 +1,23 @@
 import { SyncEvent } from './classes/sync-event';
+import { EVENT_TYPES } from './constants/event-types';
 
 export class EventProcessor {
 	private static instance: EventProcessor = new EventProcessor();
+	private eventHandlers: any = {};
+
 	public static getInstance(): EventProcessor {
 		return EventProcessor.instance;
 	}
-	
+
 	private constructor() {
-		
+		this.eventHandlers[EVENT_TYPES.PUSH] = (event: SyncEvent) => {
+
+		}
 	}
-	
+
 	public pushEvent(event: SyncEvent) {
-		
+		if (this.eventHandlers[event.type]) {
+			this.eventHandlers[event.type](event);
+		}
 	}
 }
