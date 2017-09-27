2785394884d9b0d04d7cc7368ecabe69cb187d1d
BrinkerVII <brinkervii@gmail.com>
2017-09-27 13:01:03 +0200

    Justify base path

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -7,9 +7,10 @@ export class Variables {
 
 let folderName: string = "sa-daemon-" + (new Date()).getTime().toString();
 if (process.platform === "win32") {
-	Variables.projectBasePath = path.join("%TEMP%", folderName);
+	Variables.projectBasePath = path.join(process.env.TEMP || "./tmp", folderName);
 } else {
-	Variables.projectBasePath = path.join("./tmp", folderName);
+	Variables.projectBasePath = path.join("/tmp", folderName);
 }
+path.normalize(Variables.projectBasePath);
 
 debug(`Using project base path ${Variables.projectBasePath}`);
